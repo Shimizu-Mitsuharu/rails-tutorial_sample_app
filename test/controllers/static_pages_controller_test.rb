2023f4416af6543a4b29461244ab08a304995e85
vagrant <vagrant@localhost.localdomain>
2020-05-31 05:39:17 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get FILL_IN
     assert_response FILL_IN
